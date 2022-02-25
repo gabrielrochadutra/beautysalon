35ca675848fdc181a0a4366f670c04ba192a6a68
Gabriel Dutra <gabrielrochadutra@gmail.com>
2022-02-25 15:25:13 -0300

    Aula 4

--- main.js
+++ main.js
@@ -58,6 +58,18 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `, 
   { interval: 100 })
+
+  /* BACK TO TOP */
+
+  const backToTopButton = document.querySelector('.back-to-top')
+  window.addEventListener('scroll', function() {
+    if(this.window.scrollY >=560) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+  })
